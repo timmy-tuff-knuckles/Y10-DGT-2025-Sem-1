07d9a6769188101b25cac9132f6ea626bc4356d2
timmy-tuff-knuckles <judah.thornton@kingsway.school.nz>
2025-05-09 14:13:13 +1200

    Add files via upload
    made it look cleaner and added multiplacation

--- #Input and Loops.py
+++ #Input and Loops.py
@@ -1,23 +1,36 @@
 #Input and Loops 
 #Judah Thornton
-#date: 7/05/25
-#Verstion 1
+#date: 9/05/25
+#Verstion 2
 
 #ask the user a question and store thier answer in a variable
 #ask the user for thier name and store it
+
 name = input("Kia ora, what is your name? ")
 
+#print() #this adds a line break
+#\n so does this
+
+#check that the name was stored correctly
+print(f'\n nice to meet you, {name}')
+#to comment out code use ctrl + /
+
 #ask user for 2 numbers then add them together
-num1 = int(input("what is your first number? "))
-num2 = int (input('what is youre second number? '))
-print(f'you entered your first number as {num1}')
-print(f'you entered your second number as {num2}')
+num1 = int(input("\n what is your first number? "))
+num2 = int (input('\n what is youre second number? '))
+print(f'\n you entered your first number as {num1}')
+print(f'\n you entered your second number as {num2}')
 
 #adding them together
 sum = num1 + num2
-print(f'hi {name} this is your first number added together with youre second number, {sum}')
+# numbers added
 
+print(f'\n hi {name} this is your first number added together with youre second number, {sum}')
 
+multiply = num1*num2 
+# multiplied numbers
+
+print(f'\n hi {name} this is your first number multiplied together with youre second number, {multiply}')
 #check that the name was stored correctly
 print(name)
 #to comment out code use ctrl + /
