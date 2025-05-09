0679f4ce1f2b314e68ce40c0436100c043bd62c3
timmy-tuff-knuckles <judah.thornton@kingsway.school.nz>
2025-05-09 14:46:53 +1200

    Add files via upload
    added if loops and forever loops

---
+++ coffee program (while loops).py
@@ -0,0 +1,22 @@
+#while loops
+#how to use while loops
+#judah thornton
+#9 may 2025
+# v1
+
+keep_going = "" # the variable contains an epty string
+while keep_going == '':
+
+    like_coffee = input("do you like coffee?")
+
+    if like_coffee == 'yes':
+        print('you little addict')
+        keep_going = 'done'
+
+    if like_coffee == 'no':
+        print ('same here bro')
+        keep_going = 'done'
+
+    if like_coffee == 'nahhh':
+        print ('good boy')
+        keep_going = 'done'
