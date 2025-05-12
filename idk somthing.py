bff6b4a80942145eea251590a000c7268a64ced6
timmy-tuff-knuckles <judah.thornton@kingsway.school.nz>
2025-05-12 15:03:04 +1200

    Add files via upload
    iteractive robot

---
+++ idk somthing.py
@@ -0,0 +1,53 @@
+# idk somthing
+# #judah thornton
+#12 may 25
+
+
+forever_loop = ""
+while forever_loop == '':
+
+    pick_a_number = int(input(f"1,2, or 3 pick a number\n"))
+    
+    if pick_a_number == 1:
+        awnser = (input(f'your number is 1 am I right?\n'))
+        if awnser == 'no':
+            liar = (input(f'bros def lying\n'))
+            if liar == 'no':
+                print ('stop lying')
+                forever_loop = 'a'
+            if liar == 'yes':
+                print ('thank you for being honest')
+                forever_loop = 'a'
+        if awnser == 'yes' :
+            awnser_2 = (input(f'ohh you think your fancy ay?\n'))
+            if awnser_2 == 'no':
+                print ('bros black')
+                forever_loop = 'a'
+            if awnser_2 == 'yes':
+                print ('shut up you stupid idiot')
+                forever_loop = 'black people?'
+
+
+
+    if pick_a_number == 2:
+        awnser = (input('your number is 2 am I right? '))
+        if awnser == 'no' :
+            awnser_4 = (input(f'shut up nig'))
+        if awnser == 'yes':
+            awnser_3 = (input(f'bro is so black\n'))
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    if pick_a_number == 3:
+        awnser = (input('your number is 3 am I right? '))
